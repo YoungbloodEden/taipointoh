b5feee2c786835f598d64e99d1213f5d29328159
Adam Youngblood <ayoungbl0d@gmail.com>
2018-04-30 03:59:13 -0500

    some junk shit

--- logic/youtubeLogic.js
+++ logic/youtubeLogic.js
@@ -72,6 +72,8 @@ function playback(msg, link){
   })
 }
 
+// some junk shit i need to do because heroku is giving me AIzaSyD4Yeu48Skj_Ba0KmFAbcdKL7_2gTRGESI
+
 module.exports = {
   search : search
 }
