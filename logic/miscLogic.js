92a413771a430a6edf7a75a54b7bb4cfa0a0a8f6
Adam Youngblood <ayoungbl0d@gmail.com>
2018-07-22 17:37:35 -0500

    fixed additional help function appearing

--- logic/miscLogic.js
+++ logic/miscLogic.js
@@ -8,14 +8,13 @@ function help(msg, client){
   `\`\`\`css\n
 [help:] The command you're using now.\n
 [yt:] Command for searching youtube for a video. Anything following the command will be treated as a search string. From there, you can choose which video fits your search from a list of results.\n
-[vol:] or [volume:] Command for changing the volume of Tai's playback. Takes a number between 1 and 100, rounded down.\n
 [queue:] Command to see what's in queue to play via YouTube.\n
 [qdel:] Takes a queue position (number) to remove from the current queue. \n
 [qjump:] Takes a queue position (number) to jump to the front of the queue.\n
 [qsave:] Command to save the current YouTube queue. \n
 [qget:] Command to retrieve the saved YouTube queue. \n
 [skip:] Skips the currently playing song.\n
-[volume:] Command to set the volume for playback. Takes whole numbers between 1 and 100\n
+[volume:] or [vol:] Command to set the volume for playback. Takes a number between 1 and 100, rounded down.\n
 [disc:] Command to force a disconnect from any voice channel.\n
 \`\`\``);
 }
